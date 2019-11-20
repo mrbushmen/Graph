7e283b282d2aa6839b0fb6710b926fd658e397ef
Evgeny <fakevkbushmen@gmail.com>
2019-11-20 07:38:55 +0700

    Ищет путь, но не самый короткий

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -13,20 +13,20 @@ public class Graph : MonoBehaviour
 
     private List<GraphPoint> selectedPoints = new List<GraphPoint>();
     private LineRenderer lineRenderer;
-    
+
     private int edgeCount = 0;
 
     public List<Edge> connectedPoints = new List<Edge>();
 
-    private int[,] matrix=new int[50,50];
-    private float[,] sizeMatrix=new float[50, 50];
+    private int[,] matrix = new int[50, 50];
+    private float[,] sizeMatrix = new float[50, 50];
 
     private float[] d = new float[50];
 
     //список посещенных вершин
     private List<int> passed = new List<int>();
     //список оставшихся вершин
-    private List<int> points  = new List<int>();
+    private List<int> points = new List<int>();
 
     public void FindPath()
     {
@@ -45,24 +45,86 @@ public class Graph : MonoBehaviour
             GraphPoint pointA = selectedPoints[0];
             GraphPoint pointB = selectedPoints[1];
             //TODO: поиск кратчайшего пути
+            passed.Clear();
 
-            var c = pointA.Id;
-            passed.Add(c);
+            for (int i = 0; i < 50; i++)
+            {
+                d[i] = float.MaxValue;
+            }
+
+            //Начало пути
+            int c = pointA.Id;
+            //Конец пути
+            int e = pointB.Id;
+
+            for (int i = 0; i < 50; i++)
+            {
+                //Если между вершинами есть ребро, заносим его длину в массив
+                if (matrix[c, i] == 1)
+                {
+                    d[i] = sizeMatrix[c, i];
+                }
+                else
+                {
+                    if (c == i)
+                        d[i] = 0;
+                    else
+                        d[i] = float.MaxValue;
+                }
+            }
+
             float min = float.MaxValue;
             int minC = c;
-
-            for (int i = 1; i <= GraphPoint.count; i++)
+            while (passed.Count < GraphPoint.count && c!=e)
             {
-                if (! passed.Contains(c))
+                for (int i = 0; i < GraphPoint.count; i++)
                 {
-                    if ( sizeMatrix[i, c]<min)
+                    //если расстояние до вершины меньше, чем минимальное + расстояние до текущей
+                    if (d[i] < min + d[c] && d[i] != 0 && !passed.Contains(i))
                     {
-                        min = sizeMatrix[i, c];
-                        minC = c;
+                        min = d[i];
+                        minC = i;
+                    }
+                }
+                passed.Add(c);
+                c = minC;
+                Debug.LogError(c);
+
+                for (int i = 0; i < 50; i++)
+                {
+                    //Если между вершинами есть ребро, заносим его длину в массив
+                    if (matrix[c, i] == 1 && !passed.Contains(i))
+                    {
+                        d[i] = sizeMatrix[c, i]+min;
+                    }
+                    else
+                    {
+                        if (c == i)
+                            d[i] = min;
+                        else
+                            d[i] = float.MaxValue;
                     }
                 }
             }
-            passed.Add(c);
+
+
+            //var c = pointA.Id;
+            //passed.Add(c);
+            //float min = float.MaxValue;
+            //int minC = c;
+
+            //for (int i = 1; i <= GraphPoint.count; i++)
+            //{
+            //    if (! passed.Contains(c))
+            //    {
+            //        if ( sizeMatrix[i, c]<min)
+            //        {
+            //            min = sizeMatrix[i, c];
+            //            minC = c;
+            //        }
+            //    }
+            //}
+            //passed.Add(c);
         }
     }
 
@@ -141,7 +203,7 @@ public class Graph : MonoBehaviour
         {
             GraphPoint pointA = selectedPoints[0];
             GraphPoint pointB = selectedPoints[1];
-            bool isConnected = matrix[pointA.Id,pointB.Id]==1;
+            bool isConnected = matrix[pointA.Id, pointB.Id] == 1;
 
             Debug.Log(string.Format("Deleting edge {0}, {1}...", pointA.Id, pointB.Id));
 
@@ -182,7 +244,7 @@ public class Graph : MonoBehaviour
         Debug.Log("Size= " + edge.Size);
 
         Update();
-    }    
+    }
 
     /// <summary>
     /// Удалить все ребра
@@ -200,5 +262,5 @@ public class Graph : MonoBehaviour
         }
     }
 
-    
+
 }
